4905803b8adec9400d0f19929e2a142c49755bf7
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-01-26 20:45:02 -0500

    Display data

--- worldofwarships/playerstats/views.py
+++ worldofwarships/playerstats/views.py
@@ -4,15 +4,22 @@ from django.http import HttpResponse
 from django.template import loader
 
 def get_player_data(request):
+    application_id = '586c12b8bcdeebae9fa17747f47d67ec'
+    account_id = '1005419424'
+    response = requests.get(f'https://api.worldofwarships.com/wows/account/info/?application_id={application_id}&account_id={account_id}&extra=statistics.oper_solo')
+    data = response.json()
+    context = {'data': data}
+    
     template = loader.get_template('player_stats.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context, request))
 
-    if request.method == 'GET' and 'username' in request.GET:
-        application_id = '586c12b8bcdeebae9fa17747f47d67ec'
-        account_id = '1005419424'
-        username = request.GET['username']
-        response = requests.get(f'https://api.worldofwarships.com/wows/account/info/?application_id={application_id}&account_id={account_id}&extra=statistics.oper_solo')
-        data = response.json()
-        return render(data, 'player_stats.html', {'data': data})
-    else:
-        return render(request, 'player_stats.html')
+    # if request.method == 'GET' and 'username' in request.GET:
+    #     application_id = '586c12b8bcdeebae9fa17747f47d67ec'
+    #     account_id = '1005419424'
+    #     username = request.GET['username']
+    #     response = requests.get(f'https://api.worldofwarships.com/wows/account/info/?application_id={application_id}&account_id={account_id}&extra=statistics.oper_solo')
+    #     data = response.json()
+    #     return render(data, 'player_stats.html', {'data': data})
+    # else:
+    #     return render(request, 'player_stats.html')
+    
