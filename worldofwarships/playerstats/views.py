2286f0be137a6df254fef3d3bdd7a571236e26cf
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-01-27 00:45:00 -0500

    Add Operation Combined Stats

--- worldofwarships/playerstats/views.py
+++ worldofwarships/playerstats/views.py
@@ -14,15 +14,38 @@ def get_player_data(request):
     response = requests.get(f'https://api.worldofwarships.com/wows/account/info/?application_id={application_id}&account_id={account_id}&extra=statistics.oper_div%2C+statistics.oper_solo')
     data = response.json()
 
-    data['data'][account_id]['last_battle_time'] = \
-    datetime.datetime.fromtimestamp(data['data'][account_id]['last_battle_time'])
-    data['data'][account_id]['updated_at'] = \
-    datetime.datetime.fromtimestamp(data['data'][account_id]['updated_at'])
-    data['data'][account_id]['logout_at'] = \
-    datetime.datetime.fromtimestamp(data['data'][account_id]['logout_at'])
+    account_data = data['data'][account_id] # Just for convenience and readability
+    account_data['last_battle_time'] = datetime.datetime.fromtimestamp(account_data['last_battle_time'])
+    account_data['updated_at'] = datetime.datetime.fromtimestamp(account_data['updated_at'])
+    account_data['logout_at'] = datetime.datetime.fromtimestamp(account_data['logout_at'])
+
+
+    account_stats = data['data'][account_id]['statistics']
+    account_stats['oper'] = {"wins":-1,"losses":-1,"battles":-1,"survived_wins":-1,"xp":-1,"wins_by_tasks":{"0":-1,"1":-1,"2":-1,"3":-1,"4":-1,"5":-1},"survived_battles":-1}
+
+    account_stats['oper']['battles'] = account_stats['oper_solo']['battles'] + account_stats['oper_div']['battles']
+    account_stats['oper']['wins'] = account_stats['oper_solo']['wins'] + account_stats['oper_div']['wins']
+    account_stats['oper']['losses'] = account_stats['oper_solo']['losses'] + account_stats['oper_div']['losses']
+    account_stats['oper']['survived_battles'] = account_stats['oper_solo']['survived_battles'] + account_stats['oper_div']['survived_battles']
+    account_stats['oper']['survived_wins'] = account_stats['oper_solo']['survived_wins'] + account_stats['oper_div']['survived_wins']
+    account_stats['oper']['xp'] = account_stats['oper_solo']['xp'] + account_stats['oper_div']['xp']
+    try:
+        account_stats['oper']['wins_by_tasks']['0'] = account_stats['oper_solo']['wins_by_tasks']['0'] + account_stats['oper_div']['wins_by_tasks']['0']
+    except:
+        account_stats['oper']['wins_by_tasks']['0'] = account_stats['oper_solo']['wins_by_tasks']['0']
+    try:
+        account_stats['oper']['wins_by_tasks']['1'] = account_stats['oper_solo']['wins_by_tasks']['1'] + account_stats['oper_div']['wins_by_tasks']['1']
+    except:
+        account_stats['oper']['wins_by_tasks']['1'] = account_stats['oper_solo']['wins_by_tasks']['1']
+    account_stats['oper']['wins_by_tasks']['2'] = account_stats['oper_solo']['wins_by_tasks']['2'] + account_stats['oper_div']['wins_by_tasks']['2']
+    account_stats['oper']['wins_by_tasks']['3'] = account_stats['oper_solo']['wins_by_tasks']['3'] + account_stats['oper_div']['wins_by_tasks']['3']
+    account_stats['oper']['wins_by_tasks']['4'] = account_stats['oper_solo']['wins_by_tasks']['4'] + account_stats['oper_div']['wins_by_tasks']['4']
+    account_stats['oper']['wins_by_tasks']['5'] = account_stats['oper_solo']['wins_by_tasks']['5'] + account_stats['oper_div']['wins_by_tasks']['5']
     
+    
+
+
     context = {'data': data}
-    
     template = loader.get_template('player_stats.html')
     return HttpResponse(template.render(context, request))
 
