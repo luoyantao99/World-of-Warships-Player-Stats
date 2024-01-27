e4f3657b4283f65e0de2b023be44eac2cf20370d
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-01-27 13:06:59 -0500

    Update views.py

--- worldofwarships/playerstats/views.py
+++ worldofwarships/playerstats/views.py
@@ -20,27 +20,26 @@ def get_player_data(request):
     account_stats = data['data'][account_id]['statistics']
     account_stats['oper'] = {"wins":-1,"losses":-1,"battles":-1,"survived_wins":-1,"xp":-1,"wins_by_tasks":{"0":-1,"1":-1,"2":-1,"3":-1,"4":-1,"5":-1},"survived_battles":-1}
 
-    account_stats['oper']['battles'] = account_stats['oper_solo']['battles'] + account_stats['oper_div']['battles']
-    account_stats['oper']['wins'] = account_stats['oper_solo']['wins'] + account_stats['oper_div']['wins']
-    account_stats['oper']['losses'] = account_stats['oper_solo']['losses'] + account_stats['oper_div']['losses']
-    account_stats['oper']['survived_battles'] = account_stats['oper_solo']['survived_battles'] + account_stats['oper_div']['survived_battles']
-    account_stats['oper']['survived_wins'] = account_stats['oper_solo']['survived_wins'] + account_stats['oper_div']['survived_wins']
-    account_stats['oper']['xp'] = account_stats['oper_solo']['xp'] + account_stats['oper_div']['xp']
+    combine_oper(account_stats['oper'], account_stats['oper_solo'], account_stats['oper_div'], 'battles')
+    combine_oper(account_stats['oper'], account_stats['oper_solo'], account_stats['oper_div'], 'wins')
+    combine_oper(account_stats['oper'], account_stats['oper_solo'], account_stats['oper_div'], 'losses')
+    combine_oper(account_stats['oper'], account_stats['oper_solo'], account_stats['oper_div'], 'survived_battles')
+    combine_oper(account_stats['oper'], account_stats['oper_solo'], account_stats['oper_div'], 'survived_wins')
+    combine_oper(account_stats['oper'], account_stats['oper_solo'], account_stats['oper_div'], 'xp')
 
     try: account_stats['oper_div']['wins_by_tasks']['0']
     except: account_stats['oper_div']['wins_by_tasks']['0'] = 0
     try: account_stats['oper_div']['wins_by_tasks']['1']
     except: account_stats['oper_div']['wins_by_tasks']['1'] = 0
-    account_stats['oper']['wins_by_tasks']['0'] = account_stats['oper_solo']['wins_by_tasks']['0'] + account_stats['oper_div']['wins_by_tasks']['0']
-    account_stats['oper']['wins_by_tasks']['1'] = account_stats['oper_solo']['wins_by_tasks']['1'] + account_stats['oper_div']['wins_by_tasks']['1']
-    account_stats['oper']['wins_by_tasks']['2'] = account_stats['oper_solo']['wins_by_tasks']['2'] + account_stats['oper_div']['wins_by_tasks']['2']
-    account_stats['oper']['wins_by_tasks']['3'] = account_stats['oper_solo']['wins_by_tasks']['3'] + account_stats['oper_div']['wins_by_tasks']['3']
-    account_stats['oper']['wins_by_tasks']['4'] = account_stats['oper_solo']['wins_by_tasks']['4'] + account_stats['oper_div']['wins_by_tasks']['4']
-    account_stats['oper']['wins_by_tasks']['5'] = account_stats['oper_solo']['wins_by_tasks']['5'] + account_stats['oper_div']['wins_by_tasks']['5']
-    
-    
 
+    for i in range(6):
+        combine_oper(account_stats['oper']['wins_by_tasks'], account_stats['oper_solo']['wins_by_tasks'], account_stats['oper_div']['wins_by_tasks'], f'{i}')
+
+    
     context = {'data': data}
     template = loader.get_template('player_stats.html')
     return HttpResponse(template.render(context, request))
 
+
+def combine_oper(oper, oper_solo, oper_div, field):
+    oper[field] = oper_solo[field] + oper_div[field]
