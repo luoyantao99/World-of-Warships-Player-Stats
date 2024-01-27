6ec2fbe2e44b24e8c3612824ceed8ecd66bb9a73
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-01-26 22:46:56 -0500

    Web design

--- worldofwarships/playerstats/views.py
+++ worldofwarships/playerstats/views.py
@@ -3,11 +3,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import datetime
+from django.utils import timezone
+
+
 def get_player_data(request):
     application_id = '586c12b8bcdeebae9fa17747f47d67ec'
     account_id = '1005419424'
     response = requests.get(f'https://api.worldofwarships.com/wows/account/info/?application_id={application_id}&account_id={account_id}&extra=statistics.oper_solo')
     data = response.json()
+
+    data['data'][account_id]['last_battle_time'] = \
+    datetime.datetime.fromtimestamp(data['data'][account_id]['last_battle_time'], tz=timezone.utc)
+    data['data'][account_id]['updated_at'] = \
+    datetime.datetime.fromtimestamp(data['data'][account_id]['updated_at'], tz=timezone.utc)
+    data['data'][account_id]['logout_at'] = \
+    datetime.datetime.fromtimestamp(data['data'][account_id]['logout_at'], tz=timezone.utc)
+    
     context = {'data': data}
     
     template = loader.get_template('player_stats.html')
