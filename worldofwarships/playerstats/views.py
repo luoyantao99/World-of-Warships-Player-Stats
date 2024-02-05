14d831eb8845187bb486153957376341f3ec455a
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-02-05 16:52:54 -0500

    Add avg stats

--- worldofwarships/playerstats/views.py
+++ worldofwarships/playerstats/views.py
@@ -8,6 +8,7 @@ import datetime
 def get_player_data(request):
     application_id = '586c12b8bcdeebae9fa17747f47d67ec'
     account_id = '1005419424'
+    game_modes = ['pvp','pve','rank_solo']
 
     account_json = requests.get(f'https://api.worldofwarships.com/wows/account/info/?application_id={application_id}&account_id={account_id}&extra=private.port%2Cstatistics.clan%2Cstatistics.oper_div%2Cstatistics.oper_solo%2Cstatistics.pve%2Cstatistics.rank_solo%2Cstatistics.rank_div2%2Cstatistics.rank_div3').json()
 
@@ -29,10 +30,13 @@ def get_player_data(request):
         combine_oper_stats(ship)
 
     # calculate hit ratio
-    calculate_hit_ratio(account_data['statistics'])
+    calculate_hit_ratio(account_data['statistics'], game_modes)
 
     # calculate total agro
-    calculate_total_agro(account_data['statistics'])
+    calculate_total_agro(account_data['statistics'], game_modes)
+
+    # calculate avg stats
+    calculate_avg_stats(account_data['statistics'], game_modes)
         
     
     context = {
@@ -63,8 +67,7 @@ def fill_oper_wins_by_tasks(json):
             json['oper_solo']['wins_by_tasks'][f'{i}'] = 0
 
 
-def calculate_hit_ratio(json):
-    game_modes = ['pvp','pve','rank_solo']
+def calculate_hit_ratio(json, game_modes):
     weapon_types = ['main_battery','second_battery','torpedoes']
     for mode in game_modes:
         for weapon in weapon_types:
@@ -72,7 +75,21 @@ def calculate_hit_ratio(json):
             weapon_stats['hit_ratio'] = '{:.2%}'.format(weapon_stats['hits'] / weapon_stats['shots'])
 
 
-def calculate_total_agro(json):
-    game_modes = ['pvp','pve','rank_solo']
+def calculate_total_agro(json, game_modes):
     for mode in game_modes:
         json[mode]['total_agro'] = json[mode]['art_agro'] + json[mode]['torpedo_agro']
+
+
+def calculate_avg_stats(json, game_modes):
+    for mode in game_modes:
+        battles = json[mode]['battles']
+        json[mode]['avg_damage_dealt'] = '{:.1f}'.format(json[mode]['damage_dealt'] / battles)
+        json[mode]['avg_xp'] = '{:.1f}'.format(json[mode]['xp'] / battles)
+        json[mode]['avg_frags'] = '{:.1f}'.format(json[mode]['frags'] / battles)
+        json[mode]['avg_planes_killed'] = '{:.1f}'.format(json[mode]['planes_killed'] / battles)
+
+        json[mode]['avg_agro'] = '{:.1f}'.format(json[mode]['total_agro'] / battles)
+        
+        json[mode]['avg_ships_spotted'] = '{:.1f}'.format(json[mode]['ships_spotted'] / battles)
+        json[mode]['avg_damage_scouting'] = '{:.1f}'.format(json[mode]['damage_scouting'] / battles)
+
