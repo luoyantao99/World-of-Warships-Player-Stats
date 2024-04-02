f653bf320682e3f641c8fe612b1ad6f95435ed11
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-04-02 19:06:49 -0400

    Update urls.py

--- playerstats/urls.py
+++ playerstats/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.player_search, name='player_search'),
     path('player_stats/<int:account_id>/', views.player_stats, name='player_stats'),
-    path('private_profile/<int:account_id>/', views.private_profile, name='private_profile'),
+    path('player_stats/<int:account_id>/private/', views.private_profile, name='private_profile'),
     path('search_players/', views.search_players, name='search_players'),
 ]
