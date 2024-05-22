51f359aee981c6fe3ee0aae739c8b07fcba503ab
Yantao Luo <49816287+luoyantao99@users.noreply.github.com>
2024-05-22 00:17:16 -0400

    Update save_ship_icons.py

--- data/save_ship_icons.py
+++ data/save_ship_icons.py
@@ -17,13 +17,15 @@ for ship_id, ship_info in ships_data.items():
             response = requests.get(image_url)
             
             if response.status_code == 200:
-                file_name = image_url.split('/')[-1].split('_')[0]
-                file_path = os.path.join(images_directory, file_name + '.png')
+                ship_id_str = ship_info.get('ship_id_str', 'unknown_id')
+                ship_name = ship_info.get('name', 'unknown_ship').replace(' ', '_')
+                file_name = f"{ship_id_str}_{ship_name}.png"
+                file_path = os.path.join(images_directory, file_name)
 
                 with open(file_path, 'wb') as img_file:
                     img_file.write(response.content)
                 
-                print(f"Downloaded {file_name}")
+                print(f"Downloaded {ship_id_str}")
             else:
                 print(f"Failed to download image for ship ID {ship_id}. Status code: {response.status_code}")
         except Exception as e:
